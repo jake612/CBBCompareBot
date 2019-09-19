63e3c106bf0a62e6fe58cf332ff4db26b0da657e
jake612 <jake612@live.unc.edu>
2019-09-19 11:10:06 -0400

    Test out PRAW API

--- server_control.py
+++ server_control.py
@@ -9,8 +9,10 @@ if __name__ == "__main__":
     USER_AGENT = os.getenv("USER_AGENT")
     CLIENT_SECRET = os.getenv("CLIENT_SECRET")
     CLIENT_ID = os.getenv("CLIENT_ID")
-    USERNAME = os.getenv("USERNAME")
+    REDDIT_NAME = os.getenv("REDDIT_NAME")
     PASSWORD = os.getenv("PASSWORD")
+
+    print(USER_AGENT + ", " + REDDIT_NAME + ", " + PASSWORD)
 
     # Get Reddit Instance
     r = None
@@ -18,7 +20,16 @@ if __name__ == "__main__":
         r = praw.Reddit(user_agent=USER_AGENT,
                         client_id=CLIENT_ID,
                         client_secret=CLIENT_SECRET,
-                        username=USERNAME,
+                        username=REDDIT_NAME,
                         password=PASSWORD)
     except Exception as e:
         print(e)
+
+    user = r.redditor("spez")
+
+    karma_by_subreddit = {}
+    for thing in user.submissions.top("all"):
+        subreddit=thing.subreddit.display_name
+        karma_by_subreddit[subreddit] = (karma_by_subreddit.get(subreddit, 0) + thing.score)
+
+    print(karma_by_subreddit)
